942b5a30b923d85dcc354d916b175e44d66da273
MrSatnav <69113611+MrSatnav@users.noreply.github.com>
2020-08-06 08:37:59 +0100

    Add files via upload

--- Main Program.py
+++ Main Program.py
@@ -1,21 +1,23 @@
 import tweepy
 import sqlite3
-import json
-connection = sqlite3.connect("NetworkDB.db")
-cursor=connection.cursor()
+from nltk.sentiment.util import demo_liu_hu_lexicon
+#import matplotlib
 
-auth = tweepy.AppAuthHandler("2iOmsupNqQn32DJ1EJuo1sYQz", "0kDqCC3yRqbLEQtIThXe0nwoHS9UXhoLht7R0c2VHiSt2szHPw")
-auth.set_access_token("863763963946860544-Xkas3C3b7TW8oEKNWQEOPmryiEGA3Iq", "PTGcHBqO4qFxiMD2iPFUBY1wHyCG5Tm0aTshCyhejQk2i")
-api = tweepy.API(auth)
+connection = sqlite3.connect("NetworkDB.db")
+cursor = connection.cursor()
+
+# auth = tweepy.AppAuthHandler("2iOmsupNqQn32DJ1EJuo1sYQz", "0kDqCC3yRqbLEQtIThXe0nwoHS9UXhoLht7R0c2VHiSt2szHPw")
+# auth.set_access_token("863763963946860544-Xkas3C3b7TW8oEKNWQEOPmryiEGA3Iq", "PTGcHBqO4qFxiMD2iPFUBY1wHyCG5Tm0aTshCyhejQk2i")
+# api = tweepy.API(auth)
 # for tweet in tweepy.Cursor(api.search, q='vodafone OR "tesco mobile"', lang='en', geocode='51.752022,-1.257677,100000km', result_type='recent').items(10):
 #    print(tweet.text)
 
 class APIManager:
     def __init__(self):
-        auth = tweepy.AppAuthHandler("2iOmsupNqQn32DJ1EJuo1sYQz", "0kDqCC3yRqbLEQtIThXe0nwoHS9UXhoLht7R0c2VHiSt2szHPw")
-        api = tweepy.API(auth)
+        self.__auth = tweepy.AppAuthHandler("2iOmsupNqQn32DJ1EJuo1sYQz", "0kDqCC3yRqbLEQtIThXe0nwoHS9UXhoLht7R0c2VHiSt2szHPw")
+        self.__api = tweepy.API(self.__auth)
     def searchTwitter(self):
-        public_tweets = tweepy.Cursor(api.search,q='vodafone signal OR "tesco mobile" signal OR o2 signal OR "ee" signal OR threeuk signal OR virgin signal',truncated='true',lang='en').items()
+        public_tweets = tweepy.Cursor(self.__api.search,q='vodafone signal OR "tesco mobile" signal OR o2 signal OR "ee" signal OR three signal OR virgin signal',truncated='true',locale='en').items(20)
         return public_tweets
 
 class DatabaseManager:
@@ -30,7 +32,20 @@ class DatabaseManager:
             connection.commit()
             count += 1
 
+class SentimentAnalyser:
+    def getSentiment(self, sentence):
+        demo_liu_hu_lexicon(sentence)
+
 myAPIManager = APIManager()
 myDatabaseManager = DatabaseManager()
-myDatabaseManager.addTweets(myAPIManager.searchTwitter())
+tweets = myAPIManager.searchTwitter()
+tweetTextList = []
+for tweet in tweets:
+    tweetTextList.append(tweet.text)
+mySentimentAnalyser = SentimentAnalyser()
+for tweet in tweetTextList:
+    print(tweet)
+    mySentimentAnalyser.getSentiment(tweet)
+    print('')
+#myDatabaseManager.addTweets(tweets)
 connection.close()
