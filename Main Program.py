c5bd8a512e44fdc3cf5bcbd4ad12fb7fd988dbbd
MrSatnav <69113611+MrSatnav@users.noreply.github.com>
2020-08-24 09:01:28 +0100

    Add files via upload

--- Main Program.py
+++ Main Program.py
@@ -1,3 +1,4 @@
+import json
 import tweepy
 import sqlite3
 from nltk.sentiment.util import demo_liu_hu_lexicon
@@ -15,9 +16,10 @@ cursor = connection.cursor()
 class APIManager:
     def __init__(self):
         self.__auth = tweepy.AppAuthHandler("2iOmsupNqQn32DJ1EJuo1sYQz", "0kDqCC3yRqbLEQtIThXe0nwoHS9UXhoLht7R0c2VHiSt2szHPw")
-        self.__api = tweepy.API(self.__auth)
+        self.__api = tweepy.API(self.__auth, wait_on_rate_limit=True)
     def searchTwitter(self):
-        public_tweets = tweepy.Cursor(self.__api.search,q='vodafone signal OR "tesco mobile" signal OR o2 signal OR "ee" signal OR three signal OR virgin signal',truncated='true',locale='en').items(20)
+        public_tweets = tweepy.Cursor(self.__api.search,q='vodafone -filter:retweets',geocode='55.3781,-3.4360,500km',truncated='true').items()
+        #print(public_tweets)
         return public_tweets
 
 class DatabaseManager:
@@ -26,7 +28,11 @@ class DatabaseManager:
         cursor.execute("""DELETE FROM Tweet""")
         connection.commit()
         for tweet in tweets:
-            print(tweet.text)
+            #print(tweet.text)
+            #if tweet.user.location != None:
+                #print(tweet.user.location)
+            #else:
+                #print('error')
             cursor.execute("""INSERT INTO Tweet 
                 VALUES(?,?,?,?)""", (count,tweet.text,tweet.coordinates,'vodafone'))
             connection.commit()
@@ -39,13 +45,53 @@ class SentimentAnalyser:
 myAPIManager = APIManager()
 myDatabaseManager = DatabaseManager()
 tweets = myAPIManager.searchTwitter()
-tweetTextList = []
+tweetList = []
 for tweet in tweets:
-    tweetTextList.append(tweet.text)
-mySentimentAnalyser = SentimentAnalyser()
-for tweet in tweetTextList:
-    print(tweet)
-    mySentimentAnalyser.getSentiment(tweet)
-    print('')
+    # try:
+    #     print(tweet.place.country)
+    # except:
+    #     try:
+    #         print(tweet.user.location)
+    #     except:
+    #         print('none')
+    try:
+        if tweet.place != None:
+            print(tweet.place)
+        else:
+            if tweet.user.location != None:
+                print(tweet.user.location)
+            else:
+                print('None')
+    except:
+        print(tweet)
+    # try:
+    #     if tweet.place.country == 'United Kingdom':
+    #         print(tweet.place)
+    # except:
+    #     try:
+    #         print(tweet.user.location)
+    #     except:
+    #     #     print('none')
+    #         pass
+
+    # try:
+    #     print(tweet)
+    #     print(tweet.place.country)
+    #     if tweet.place.country == 'United Kingdom':
+    #         tweetList.append([tweet.text,tweet.place.full_name])
+    # except:
+    #     pass
+
+
+#print(tweetList)
 #myDatabaseManager.addTweets(tweets)
+#tweetTextList = []
+# for tweet in tweets:
+#     tweetTextList.append(tweet.text)
+# mySentimentAnalyser = SentimentAnalyser()
+# for tweet in tweetTextList:
+#     print(tweet)
+#     mySentimentAnalyser.getSentiment(tweet)
+#     print('')
+
 connection.close()
