c40b39871d422a8bf7d5301c253b8bc30fc5ae3f
MrSatnav <69113611+MrSatnav@users.noreply.github.com>
2020-10-15 21:09:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,64 +6,73 @@ from textblob import TextBlob
 import map
 import time
 
-conn = sqlite3.connect('twitterDB.db')
-sql_cursor = conn.cursor()
+TWITTER_APP_KEY = "2iOmsupNqQn32DJ1EJuo1sYQz"
+TWITTER_APP_SECRET = "0kDqCC3yRqbLEQtIThXe0nwoHS9UXhoLht7R0c2VHiSt2szHPw"
 
-auth = tweepy.OAuthHandler("2iOmsupNqQn32DJ1EJuo1sYQz", "0kDqCC3yRqbLEQtIThXe0nwoHS9UXhoLht7R0c2VHiSt2szHPw")
-auth.set_access_token("863763963946860544-Xkas3C3b7TW8oEKNWQEOPmryiEGA3Iq",
-                      "PTGcHBqO4qFxiMD2iPFUBY1wHyCG5Tm0aTshCyhejQk2i")
+auth = tweepy.OAuthHandler(TWITTER_APP_KEY, TWITTER_APP_SECRET) # Authentication for use of Twitter API.
+                                                                # Keys provided when creating a Twitter Application
+                                                                # as a Twitter Developer
 api = tweepy.API(auth)
 
+conn = sqlite3.connect('twitterDB.db') # Connect to sqlite3 database
+sql_cursor = conn.cursor()
 
 def get_data():  # Loop over each county (region) and request for tweets within it
     all_regions = sql_cursor.execute("""SELECT ctyua17nm, lat, long, st_areashape
                                 FROM Counties_and_Unitary_Authorities__December_2017__Boundaries_UK""").fetchall()
-                                # Get the relevant information from every county in the database
+                                # Get the relevant information from every county (named as 'regions') in the database
     rate_limit_reached = False
     count = 0
-    while count < len(all_regions):
-        county = all_regions[count]
-        region, lat, long, area = county[0], county[1], county[2], county[3]
+    while count < len(all_regions): # Loop over each county
+        region = all_regions[count]
+        region, lat, long, area = region[0], region[1], region[2], region[3]
         public_tweets = get_tweets_for_region(region, lat, long, area)
-        while not rate_limit_reached:
+        while not rate_limit_reached: # Loop over each tweet received and insert it into the database
             try:
                 tweet = public_tweets.next()
                 add_to_table(tweet, region)
-            except tweepy.TweepError:
+            except tweepy.TweepError: # If the rate limit has been reached (450 requests made), raise an exception
                 rate_limit_reached = True
-            except StopIteration:
+            except StopIteration: # If we have finished iterating over all the tweets in public_tweets
                 break
 
         if rate_limit_reached:
             print("Error, max requests exceeded")
-            time.sleep(60 * 15)
-            continue
+            map.make_map() # Show the map
+            time.sleep(60 * 15) # Wait 15 minutes for rate limit to run out. After it runs out,
+                                # the last county searched is searched again, and it continues from there.
+            rate_limit_reached = False
         else:
             count += 1
         print(count)
 
 
-def get_tweets_for_region(region, lat, long, area):
+def get_tweets_for_region(region, lat, long, area): # Makes the request to the API for tweets within a specific area
     print(region)
-    radius = math.sqrt(area / math.pi) / 1000
+    radius = math.sqrt(area / math.pi) / 1000 # The Twitter API allows you to search for tweets within a radius of a
+                                              # lat and long. Here the radius is approximated from the area of the
+                                              # region.
     geocode = str(str(lat) + "," + str(long) + "," + str(radius) + "km")
     public_tweets = tweepy.Cursor(api.search,
                                   q='vodafone -filter:retweets',
                                   geocode=geocode,
-                                  count=100, tweet_mode='extended').items()
+                                  tweet_mode='extended').items()
+                                    # searches for tweets with the keyword vodafone in their text,
+                                    # filtering out retweets, and in extended mode, which returns the full tweet text
     return public_tweets
 
 
-def add_to_table(tweet, region):
+def add_to_table(tweet, region): # Parse the tweet JSON and insert it into the sqlite3 table
     json_str = json.dumps(tweet._json)
     parsed = json.loads(json_str)
+
     # tweet attributes
     id_str = parsed['id']
-    # if  parsed['full_text'] is not None:
     text = parsed['full_text']
     sentiment = TextBlob(text).sentiment
-    polarity = sentiment.polarity
-    subjectivity = sentiment.subjectivity
+    polarity = sentiment.polarity # how positive or negative the tweet is
+    subjectivity = sentiment.subjectivity # subjective sentences generally refer to personal opinion,
+                                          # emotion or judgment whereas objective refers to factual information
     created_at = parsed['created_at']
     retweet_count = parsed['retweet_count']
     favorite_count = parsed['favorite_count']  # nullable
@@ -94,7 +103,7 @@ def add_to_table(tweet, region):
                             user_id,
                             region))
         conn.commit()
-    except sqlite3.IntegrityError:
+    except sqlite3.IntegrityError: # if the tweet is already in the database, discard it.
         pass
     try:
         sql_cursor.execute("""INSERT INTO users VALUES (?,?,?,?,?,?,?,?,?,?)""",
--- map.py
+++ map.py
@@ -6,24 +6,9 @@ import random
 import dataset
 db = dataset.connect("sqlite:///twitterDB.db")
 
-cdict = {'red':   [(0.0,  0.0, 0.0),
-                   (0.5,  1.0, 1.0),
-                   (1.0,  1.0, 1.0)],
-
-         'green': [(0.0,  0.0, 0.0),
-                   (0.25, 0.0, 0.0),
-                   (0.75, 1.0, 1.0),
-                   (1.0,  1.0, 1.0)],
-
-         'blue':  [(0.0,  0.0, 0.0),
-                   (0.5,  0.0, 0.0),
-                   (1.0,  1.0, 1.0)]}
-#cmap = cm.colors.LinearSegmentedColormap(cdict['red'], cdict['green'])
-
-pio.renderers.default = 'browser'
-
 uk_regions = json.load(open("ukcounties.json", 'r'))
 d = {'id': [], 'tweets': [], 'averageSent': [], 'count': []}
+
 def make_map():
     for feature in uk_regions['features']:
         feature['id'] = feature['properties']['NAME_2']
@@ -45,16 +30,11 @@ def make_map():
         if count > 0:
             averageSent = totalSent/count
             d['averageSent'].append(averageSent)
-            #d['averageSent'].append(random.uniform(1, -1))
         else:
-            #d['averageSent'].append(random.uniform(1, -1))
             d['averageSent'].append(0.0)
-
-        #d['num'].append(random.randint(1,100))
 
     df = pd.DataFrame(d)
 
-    #df['id'] = df['State or union territory'].apply(lambda x: region_num_map[x])
     fig = go.Figure(go.Choroplethmapbox(geojson=uk_regions, locations=df['id'],
                                z=df['averageSent'], zmin=-1, zmax=1,
                                colorscale= "viridis", #["red", "white", "green"], #[(0,"red"), (0.4,"lightred"), (0.5,"yellow"), (0.6,"lightgreen"), (1,"green")],
